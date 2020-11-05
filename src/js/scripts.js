c8021c0db1fc77b8fac7364b1618f5275daa20b0
alex-suschin <alex_suschin@mail.ru>
2020-11-05 11:33:28 +0300

    full-desk

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,22 @@ $(document).scroll(() => {
 
 // example scrollTo
 $(document).ready(function() {
+    $("input[type=tel]").mask("+7 (999) 999-9999");
+
+    $('.top-menu a').click(function() {
+        var scroll_elem = $(this).attr('href');
+        if ($(scroll_elem).length != 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_elem).offset().top - 50
+            }, 800);
+        } else {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 800);
+        }
+        return false;
+    });
+
     $('.header').on("click", "a", (e) => {
         e.preventDefault()
         let id = $(this).attr('href'),
@@ -31,6 +47,13 @@ $(document).ready(function() {
         navigation: {
             nextEl: '.swiper-button-next-services',
             prevEl: '.swiper-button-prev-services',
+        }
+    });
+
+    var mySwiperRew = new Swiper('.rewiews-slider-container', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination-rewiews',
         }
     });
 });
